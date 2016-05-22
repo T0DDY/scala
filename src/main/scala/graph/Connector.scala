33c9d8ab61c43586c8245b7f8acc887c722393b7
T0DDY <lyt3721@gmail.com>
2016-05-22 17:21:06 +0800

    modify connector

--- src/main/scala/graph/Connector.scala
+++ src/main/scala/graph/Connector.scala
@@ -9,6 +9,8 @@ import org.apache.hadoop.hbase.TableName
 import org.apache.hadoop.hbase.HTableDescriptor
 import org.apache.hadoop.hbase.HColumnDescriptor
 import org.apache.spark.graphx._
+import org.apache.hadoop.hbase.client.{ HBaseAdmin, HTable, Put, Get }
+import org.apache.hadoop.hbase.util.Bytes
 
 /**
  * @author liyingtao
@@ -20,35 +22,47 @@ object Connector {
     conf.set("spark.hbase.host", "172.17.0.2")
 
     val sc = new SparkContext(conf)
-    print("init running env finished \n")
+    print("init env finished \n")
+
+    //    anouther way
+    val hbaseconf = HBaseConfiguration.create()
+    hbaseconf.set("hbase.zookeeper.property.clientPort", "2181")
+    hbaseconf.set("hbase.zookeeper.quorum", "172.17.0.2")
+    hbaseconf.set("hbase.master", "172.17.0.2:60000");
+//
+//    val conn = ConnectionFactory.createConnection(hbaseconf)
+//
+//    val table = TableName.valueOf("test_table")
+//    val tableDesc = new HTableDescriptor(table)
+//    tableDesc.addFamily(new HColumnDescriptor("f".getBytes))
+//    print("creating...\n")
+//
+//    val admin = conn.getAdmin
+//
+//    if (admin.tableExists(table)) {
+//      admin.disableTable(table)
+//      admin.deleteTable(table)
+//    }
+//    admin.createTable(tableDesc)
+//    print("Created!\n")
 
     // a way
-    val rdd = sc.parallelize(1 to 100).map(i => (i.toString(), i + 1, "hello"))
-
-    rdd.toHBaseTable("test_table").toColumns("column1", "column3").inColumnFamily("f").save()
-
-    // anouther way
-    //    val hbaseconf = HBaseConfiguration.create()
-    //    hbaseconf.set("hbase.zookeeper.property.clientPort", "2181")
-    //    hbaseconf.set("hbase.zookeeper.quorum", "172.17.0.2")
-    //
-    //    val conn = ConnectionFactory.createConnection(hbaseconf)
-    //
+//    val rdd = sc.parallelize(1 to 100).map(i => (i.toString(), i + 1, "hello"))
+//
+//    print("start to insert data...\n")
+//    rdd.toHBaseTable("test_table").toColumns("column1", "column3").inColumnFamily("f").save()
+//
+//    print("Insert data finished!\n")
+    
+    print("start query data...\n")
     //    val table = TableName.valueOf("test_table")
-    //    val tableDesc = new HTableDescriptor(table)
-    //    tableDesc.addFamily(new HColumnDescriptor("f".getBytes))
-    //    print("creating...\n")
-    //
-    //    val admin = conn.getAdmin
-    //
-    //    if (admin.tableExists(table)) {
-    //      admin.disableTable(table)
-    //      admin.deleteTable(table)
-    //    }
-    //    admin.createTable(tableDesc)
-    //    print("Created!\n")
 
-    print("Insert data finished!\n")
+    val theget = new Get(Bytes.toBytes("10001"))
+    val result = (new HTable(hbaseconf, "test_table")).get(theget)
+//    val value = result.value()
+    print("queried value:"+result.toString()+"\n")
+
+    print("query finished.\n")
 
   }
 }
